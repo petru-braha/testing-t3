c91d21b437700ac3549ba245d5b361aee652d5dc
p <petrubraha@gmail.com>
2025-06-15 15:16:27 +0300

    HOPA

--- app/index.php
+++ app/index.php
@@ -9,3 +9,5 @@ $array = array("1" => "PHP code tester Sandbox Online",
 foreach( $array as $key => $value ){
     echo $key."\t=>\t".$value."\n";
 }
+
+echo "HOPAHOPAHOPAHOPAHOPAHOPAHOPAHOPAHOPAHOPAHOPAHOPAHOPAHOPAHOPA\n";
